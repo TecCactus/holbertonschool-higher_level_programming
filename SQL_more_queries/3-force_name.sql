ccbc3f11f539d9f161762a15bf2986c928380c2d
TecCactus <dm0567814@gmail.com>
2026-02-18 00:52:33 +0000

    Task 3 force name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
--- Creates table force_name on hbtn_0d_2 with id and name (name cannot be NULL)
-CREATE TABLE IF NOT EXISTS hbtn_0d_2.force_name (
+-- Creates table force_name with id INT and name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS force_name (
   id INT,
   name VARCHAR(256) NOT NULL
 );
