1d3045eb9990789da1026df0c7f8e925b5c59815
TecCactus <dm0567814@gmail.com>
2026-02-18 00:57:56 +0000

    Fix task 9 cities by states

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- Lists all cities with their states using a single SELECT
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states
+ON cities.state_id = states.id
+ORDER BY cities.id ASC;
